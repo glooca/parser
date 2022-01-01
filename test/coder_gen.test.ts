4473e19e347319d3011f51f4082fa9e01fd0eaf4
glooca <47379602+glooca@users.noreply.github.com>
2022-01-01 23:59:41 +0200

    Add callback tests, check generator bytes in tests

--- test/coder_gen.test.ts
+++ test/coder_gen.test.ts
@@ -1,6 +1,5 @@
 import { assertEquals } from "https://deno.land/std@0.118.0/testing/asserts.ts";
 import {
-  Coder,
   coderFactory,
   Cursor,
   nullTermStr,
@@ -9,61 +8,78 @@ import {
   u32,
 } from "../mod.ts";
 
-Deno.test("interface coder factory generator", async () => {
+Deno.test("coder factory generator", async () => {
   interface TestItem {
     version: number;
     data: TestItemData;
   }
-  const testItemCoder: Coder<TestItem> = coderFactory((r) => {
+  let callbackVersionInfo: number | undefined;
+  const testItemCoder = coderFactory<TestItem>((r) => {
     r(u16(), "version");
-    r((item) => testItemDataCoder(item.version ?? 0), "data");
+    r((header) => {
+      callbackVersionInfo = header.version;
+    });
+    r((item) => testItemDataCoder(item.version!), "data");
   });
+
   interface TestItemData {
     myProp: number;
     versionDependentData: string;
   }
-  function testItemDataCoder(version: number): Coder<TestItemData> {
-    return coderFactory((r) => {
+  function testItemDataCoder(version: number) {
+    return coderFactory<TestItemData>((r) => {
       r(u32(), "myProp");
       if (version > 3) {
-        r(nullTermStr as Coder<string>, "versionDependentData");
+        r(nullTermStr, "versionDependentData");
       } else {
-        r(u16LenStr() as Coder<string>, "versionDependentData");
+        r(u16LenStr(), "versionDependentData");
       }
     });
   }
 
   const testStr = "Hello, World!";
-  const testStrByteLength = new TextEncoder().encode(testStr).length;
-  const testDataList: [TestItem, number][] = [
-    [
-      {
+  const testStrBytes = new TextEncoder().encode(testStr);
+
+  const testDataList: { testData: TestItem; expectedBytes: number[] }[] = [
+    {
+      testData: {
         version: 4,
         data: {
           myProp: 1234,
           versionDependentData: testStr,
         },
       },
-      4 /* u32 */ + 2 /* pad */ + testStrByteLength + 1 /* null byte */,
-    ],
-    [
-      {
+      expectedBytes: [
+        ...[0, 4] /* u16 */,
+        ...[0, 0, 4, 210] /* u32 */,
+        ...[...testStrBytes, 0 /* null byte */],
+      ],
+    },
+    {
+      testData: {
         version: 2,
         data: {
           myProp: 342,
           versionDependentData: testStr,
         },
       },
-      4 /* u32 */ + 2 /* pad */ + 2 /* u16 len */ + testStrByteLength,
-    ],
+      expectedBytes: [
+        ...[0, 2] /* u16 */,
+        ...[0, 0, 1, 86] /* u32 */,
+        ...[...[0, testStrBytes.byteLength] /* u16 len */, ...testStrBytes],
+      ],
+    },
   ];
 
   for (let i = 0; i < testDataList.length; i++) {
-    const [testData, expectedByteSize] = testDataList[i];
+    const { testData, expectedBytes } = testDataList[i];
     const data = await testItemCoder.encode(testData);
-    assertEquals(data.length, expectedByteSize);
+    assertEquals(callbackVersionInfo, testData.version);
+    callbackVersionInfo = undefined;
+    assertEquals(data, new Uint8Array(expectedBytes));
     const cursor = new Cursor(0);
     assertEquals(await testItemCoder.decode(data, cursor), testData);
+    assertEquals(callbackVersionInfo, testData.version);
     assertEquals(cursor.index, data.length);
   }
 });
